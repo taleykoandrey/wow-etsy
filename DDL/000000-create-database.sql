55c8b9d28ed03bafa4405446584a8653cf8c89d6
andrey taleyko <andreytaleyko@yandex.ru>
2016-11-16 09:36:41 +0300

    DDL
    initial ddl scripts

---
+++ DDL/000000-create-database.sql
@@ -0,0 +1,10 @@
+/*
+@brief
+Создание БД J8. 
+*/
+ 
+CREATE DATABASE j8
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       LC_COLLATE = 'Russian_Russia.1251'
+       LC_CTYPE = 'Russian_Russia.1251';
